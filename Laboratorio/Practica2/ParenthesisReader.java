f9511dbc0d0c415545c79e3faa1744deef3f7ee2
Alberto Montes <alm59321@gmail.com>
2013-10-04 09:43:48 +0200

    Some classes ended

--- Laboratorio/Practica2/ParenthesisReader.java
+++ Laboratorio/Practica2/ParenthesisReader.java
@@ -5,65 +5,74 @@ import java.io.*;
 public class ParenthesisReader {
 	public static void main(String[] args) throws IOException {
 		Deque<Character> stack = new ArrayDeque<Character>();
-		DataInputStream in = null;
+		BufferedReader br = null;
         DataOutputStream out = null;
-        char[] chars = new char[1024];
+        String text = "";
+        String line;
         int pos = 0;
         char c = (char) 0;
+        boolean ok = true;
         
         try {
 			// Initialize with the standard Input/OutputStream from computer: keyboard and console.
-            in = new DataInputStream(System.in);
+            br = new BufferedReader(new InputStreamReader(System.in));
             out = new DataOutputStream(System.out);
             
-            while((c = in.readChar()) != -1) chars[pos++] = c;
-        } catch (EOFException e) {
+            while ((line = br.readLine()) != null) text += line;
+            
+        } catch (Exception e) {
         	
         } finally {
-        	int size = pos+1;
+        	int size = text.length();
         	try {
 	        	for (int i = 0; i < size; i++) {
-	        		c = chars[i];
+	        		c = text.charAt(i);
 	        		if (c == '[' || c == '(' || c == '{')
 						stack.push(c);
 	        		else if (c == ']' || c == ')' || c == '}') {
 						char d = stack.pop();
+						int aux = 0;
+	    				if ( (((int)c) % 5) != 1) 
+	    					aux = 1;
+	    	        	char dd = (char) (c - 1 - aux);
 						// dismatch
-						if ( c != d) {
-							System.out.println("Left parenthesis " + c + " don't match with " + d + ".");
+						if ( dd != d) {
+							System.out.println("Left parenthesis " + d + " don't match with " + c + ".");
+							ok = false;
 						}
 					}
 	        	}
-        	} catch(EmptyStackException e) {
+        	} catch(Exception e) {
     			// catch the Stack was empty exception
-            	if (e instanceof EmptyStackException) {
-    	        	int aux = ((int)c)%2;
+            	if (e instanceof NoSuchElementException) {
+    	        	int aux = 0;
+    				if ( (((int)c) % 5) != 1) 
+    					aux = 1;
     	        	char d = (char) (c - 1 - aux);
     	        	System.out.println("Missing left parenthsis " + d + ".");
+    	        	ok = false;
             	}
-            	if (in != null) {
-                    in.close();
-                }
-                if (out != null) {
-                    out.close();
-                }
     		} finally {
     			// look if elements remain in the Stack
-    			if (stack.size() == 0) System.out.println("OK");
+    			if (stack.size() == 0 && ok) System.out.println("OK");
     			else if (stack.size() > 3) System.out.println("Too many nested parentheses.");
-    			else  {
+    			else if (stack.size() != 0){
     				c = stack.pop();
     				int aux = ((int)c)%2;
     				char d = (char) (c + 1 + aux);
     				System.out.println("Missing right parentheses " + d + ".");
     			}
     		}
+        	try {
+	        	if (br != null) {
+	                br.close();
+	            }
+	            if (out != null) {
+	                out.close();
+	            }
+        	} catch(Exception e) {}
         }
-        
-        
-		
 	}
-	
 	
 }
 
